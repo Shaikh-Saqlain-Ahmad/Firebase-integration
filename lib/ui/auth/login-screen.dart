ac0ffa664511d32b4d0e066200d852b95ed5f403
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-08-23 23:51:25 +0500

    login screen created

--- lib/ui/auth/login-screen.dart
+++ lib/ui/auth/login-screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/widgets/round-button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,14 +9,78 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey =
+      GlobalKey<FormState>(); //to validate whether the field is empty or not
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController
+        .dispose(); //to remove from memory after closing of screen
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login screen'),
         automaticallyImplyLeading: false,
       ),
-      body: Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: 'Email',
+                            helperText: 'bakhtiar@gmail.com',
+                            prefixIcon: Icon(Icons.email)),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter email';
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: 'Password',
+                              prefixIcon: Icon(Icons.lock)),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter password';
+                            } else {
+                              return null;
+                            }
+                          })
+                    ],
+                  )),
+              SizedBox(
+                height: 50,
+              ),
+              RoundButton(
+                title: 'Login',
+                ontap: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+              )
+            ]),
+      ),
     );
   }
 }
